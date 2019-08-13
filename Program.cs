60cd6dc0701efcf42d5654e2e7a5da8a73bfe51a
ralph.headley <ralph.headley@ajbell.co.uk>
2019-08-13 08:41:52 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheSupermarketQueue
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine(QueueTime(new int[] { 10, 2, 3, 3 }, 2).ToString());
+            //Console.WriteLine(QueueTime(new int[] { 1, 2, 3, 4 }, 1).ToString());
+        }
+
+        public static long QueueTime(int[] customers, int n)
+        {            
+            // customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+            // n: a positive integer, the number of checkout tills.
+
+            // There is only ONE queue serving many tills, and
+            // The order of the queue NEVER changes, and
+            // The front person in the queue(i.e.the first element in the array/ list) proceeds to a till as soon as it becomes free.            
+
+            List<int> tills = new List<int>();
+            for (int i = 0; i < n; i++) {
+                tills.Add(0);
+            }
+
+            foreach (int customer in customers)
+            {
+                Console.WriteLine(customer);
+                Console.WriteLine(tills.Min());
+                tills[tills.IndexOf(tills.Min())] = tills.Min() + customer;
+                Console.WriteLine(tills.Min());
+            }                  
+
+            return tills.Max();
+        }
+
+    }
+}
---
+++ tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TheSupermarketQueue
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ExampleTest1()
+        {
+            long expected = 0;
+
+            long actual = Program.QueueTime(new int[] { }, 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExampleTest2()
+        {
+            long expected = 10;
+
+            long actual = Program.QueueTime(new int[] { 1, 2, 3, 4 }, 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExampleTest3()
+        {
+            long expected = 9;
+
+            long actual = Program.QueueTime(new int[] { 2, 2, 3, 3, 4, 4 }, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExampleTest4()
+        {
+            long expected = 5;
+
+            long actual = Program.QueueTime(new int[] { 1, 2, 3, 4, 5 }, 100);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExampleTest5()
+        {
+            long expected = 12;
+
+            long actual = Program.QueueTime(new int[] { 5, 3, 4 }, 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExampleTest6()
+        {
+            long expected = 10;
+
+            long actual = Program.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
